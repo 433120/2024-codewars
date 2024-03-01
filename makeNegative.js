ffac3260715ac4830d70425742eb7c434f0ac6dd
433120 <herry.soewito@gmail.com>
2024-03-01 22:35:46 +0700

    Codewars Make Negative

---
+++ makeNegative.js
@@ -0,0 +1,8 @@
+// Codewars.com
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+// function makeNegative(num) {
+//     return -Math.abs(num);
+//   }
+makeNegative = (num) => -Math.abs(num);
+console.log(makeNegative(9))
