8df2fd9edb3aad19856657cf34b7634553e189df
433120 <herry.soewito@gmail.com>
2024-03-01 22:22:24 +0700

    Codewars multiply

---
+++ multiply.js
@@ -0,0 +1,9 @@
+// Codewars.com
+//https://www.codewars.com/kata/50654ddff44f800200000004/javascript
+
+// function multiply(a, b){
+//    return  a * b
+//   }
+// const multiply=(a,b)=>a*b;
+multiply = (a,b) => a*b;
+console.log(multiply(4,7))
